bc25c86e542ab212d61ffd06aeb118647e5288c4
Felipe <felipe.silva.hts@gmail.com>
2021-03-04 17:23:39 -0300

    Aula 54

---
+++ EFCore.Repo/EFCoreRepositorie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Repo
+{
+    class EFCoreRepositorie : IEFCoreRepository
+    {
+        private readonly HeroiContexto _context;
+
+        public EFCoreRepositorie(HeroiContexto context)
+        {
+            _context = context;
+        }
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public Task<bool> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
